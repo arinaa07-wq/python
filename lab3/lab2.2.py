0fddf08b327250bdc48abb99887f2e938412c4cc
arinaa07-wq <arinaa07-wq@users.noreply.github.com>
2026-03-15 22:26:12 +0530

    lab3

--- lab3/lab2.2.py
+++ lab3/lab2.2.py
@@ -1,4 +1,11 @@
 def f(a):
-    rez=""
     if a==1 or a==0: return 1
+    a0=1
+    a1=1
+    a_n=0
+    for n in range(2, a + 1):
+        a_n=a0+a0/(2**(n-2))
+        a0,a1=a1,a_n
+    return a_n if a>1 else 1
+print(f(5))
     
