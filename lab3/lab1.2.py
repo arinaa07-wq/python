5c047f27cdc45fcd8321364cf5900113897147ad
arinaa07-wq <arinaa07-wq@users.noreply.github.com>
2026-03-16 15:50:15 +0530

    lab3.1.2

--- lab3/lab1.2.py
+++ lab3/lab1.2.py
@@ -1,5 +1,5 @@
-def f(a):
+def g(a):
     if a==0 or a==1: return 1
-    else: return f(a-2)+f(a-1)/(2**(a-1))
+    else: return g(a-2)+g(a-1)/(2**(a-1))
 for a in range(6):
-    print(f"a_{a} = {f(a)}")
+    print(f"a_{a} = {g(a)}")
