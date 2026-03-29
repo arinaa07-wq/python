0ac7f3c5f6bdd40a08df4dd62072d62f09ebe9f1
arinaa07-wq <arinaa07-wq@users.noreply.github.com>
2026-03-30 00:17:27 +0530

    lab2

--- lab2/11.py
+++ lab2/11.py
@@ -1,5 +1,13 @@
+import doctest
 from itertools import*
 def f():
+    """
+    вычисление сколько кодов можно составить из слова настя,
+    при условии что главная входит не более одного раза
+    
+    >>> f()
+    6075
+    """
     k=0
     alf='НАСТЯ'
     for x in product(alf, repeat=6):
@@ -8,3 +16,4 @@ def f():
             k=k+1
     return(k)        
 print(f())
+doctest.testmod(verbose=True)
